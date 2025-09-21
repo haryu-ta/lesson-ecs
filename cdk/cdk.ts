e74c1dbf62e04f4b506cb502b1358e9621d44740
Itamura.Ryohei <ita@example.com>
2025-09-21 11:10:08 +0900

    完成版

--- cdk/cdk.ts
+++ cdk/cdk.ts
@@ -32,7 +32,7 @@ export class FargateSimpleStack extends cdk.Stack {
       description: 'Allow HTTP traffic to Fargate tasks',
       securityGroupName: 'FargatePublicSG',
     });
-    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP from anywhere');
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'Allow HTTP from anywhere');
 
     // 実行ロールを作成
     const executionRole = new iam.Role(this, 'EcsExecutionRole', {
